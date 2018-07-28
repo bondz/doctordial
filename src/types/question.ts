f6aad7a32fd17dc9efce77fa37068ed947435d23
Bond <bondz@outlook.com>
2018-07-28 21:14:01 +0100

    Create user and question graphql types

---
+++ src/types/question.ts
@@ -0,0 +1,68 @@
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectTypeConfig,
+} from 'graphql';
+
+import UserType from './user';
+import User from '../models/user';
+import { IQuestion, IAnswer } from '../models/questions-bank';
+
+const QuestionType = new GraphQLObjectType(<
+  GraphQLObjectTypeConfig<IQuestion, any>
+>{
+  name: 'QuestionType',
+  description: 'Describes a question asked in the doctordial service',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+      description:
+        'The ID of the question, can be used to fetch the question for other actions',
+    },
+    text: {
+      type: GraphQLString,
+      description: 'The content of the question',
+    },
+    author: {
+      type: UserType,
+      description: 'The user who created the question',
+      resolve: question => User[question.author],
+    },
+    answers: {
+      type: new GraphQLList(AnswerType),
+      description: 'The answers provided for this question',
+    },
+  }),
+});
+
+const AnswerType = new GraphQLObjectType(<
+  GraphQLObjectTypeConfig<IAnswer, any>
+>{
+  name: 'AnswerType',
+  description: 'Describes the answer to a question in the doctordial service',
+  fields: () => ({
+    author: {
+      type: UserType,
+      description: 'The user who created this answer',
+      resolve: answer => {
+        return null;
+      },
+    },
+    text: {
+      type: GraphQLString,
+      description: 'The answer to the question',
+    },
+    upvotes: {
+      type: GraphQLInt,
+      description: 'The number of upvotes this question has',
+    },
+    downvotes: {
+      type: GraphQLInt,
+      description: 'The number of downvotes this question has',
+    },
+  }),
+});
+
+export default QuestionType;
---
+++ src/types/user.ts
@@ -0,0 +1,41 @@
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLObjectTypeConfig,
+} from 'graphql';
+import { IUser } from '../models/user';
+
+const UserType = new GraphQLObjectType(<GraphQLObjectTypeConfig<IUser, any>>{
+  name: 'UserType',
+  description:
+    'Describes a user (patient or doctor) who signs up using the doctordial service',
+  fields: () => ({
+    firstName: {
+      type: GraphQLString,
+      description: 'The first name of the user',
+    },
+    lastName: {
+      type: GraphQLString,
+      description: 'The last name of the user',
+    },
+    isPatient: {
+      type: GraphQLBoolean,
+      description: 'Specifies if the user has a patient account',
+    },
+    isDoctor: {
+      type: GraphQLBoolean,
+      description: 'Specifies if the user has a doctor accoutn',
+    },
+    accountVerified: {
+      type: GraphQLBoolean,
+      description: 'Specifies if the user account has been verified',
+    },
+    isVerifiedDoctor: {
+      type: GraphQLBoolean,
+      description: 'Specifies if the doctor account has been verified',
+    },
+  }),
+});
+
+export default UserType;
