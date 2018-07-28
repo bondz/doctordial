c1a702bbaef8abcdd80cd579f9e3f987e8704691
Bond <bondz@outlook.com>
2018-07-28 21:13:07 +0100

    Seed user model with samples

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,4 @@
-interface User {
+export interface IUser {
   firstName: string;
   lastName: string;
   isDoctor: boolean;
@@ -10,6 +10,40 @@ interface User {
   password: string;
 }
 
-const users: User[] = [];
+const users: IUser[] = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    isDoctor: true,
+    isPatient: false,
+    accountVerified: true,
+    isVerifiedDoctor: true,
+    email: 'john@example.com',
+    phone: '+17032455890',
+    password: 'an-encrypted-string',
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    isDoctor: false,
+    isPatient: true,
+    accountVerified: true,
+    isVerifiedDoctor: false,
+    email: 'doe@example.com',
+    phone: '+2348054231245',
+    password: 'a-different-encrypted-string',
+  },
+  {
+    firstName: 'Maddie',
+    lastName: 'McAvoy',
+    isDoctor: true,
+    isPatient: false,
+    accountVerified: true,
+    isVerifiedDoctor: true,
+    email: 'maddie@example.gov',
+    phone: '+19012339402',
+    password: '-no-phrase(word)-is-safe!',
+  },
+];
 
 export default users;
