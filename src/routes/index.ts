84087267503eca61274f02c688d0aa680b0b0e84
Bond <bondz@outlook.com>
2018-07-29 15:34:04 +0100

    Setup logging endpoint for sending client side logs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 
 import { signupHandler, loginHandler } from '../controllers/auth';
+import logger from '../logger';
 
 const router = Router();
 
 router.post('/login', loginHandler);
 router.post('/signup', signupHandler);
+router.post('/log', (req, res) => {
+  logger.debug(req.body, 'Client logs');
+
+  res.status(200).send('ok');
+});
 
 export default router;
